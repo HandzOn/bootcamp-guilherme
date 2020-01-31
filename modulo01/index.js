f5501884b53495a6d17d007a4b594c4d8573c648
Guilherme França <guii_real@hotmail.com>
2020-01-31 00:36:36 -0300

    CRUD users

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,12 +2,40 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+
 // Query params = ?teste=1
 // Route params = /users/1
 // Request body = { test: 1 }
 
-server.get('/teste', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` });
+const users = ['Guilherme', 'Erisvaldo', 'William']
+
+server.get('/api/users', (req, res) => {
+    return res.json(users);
+})
+
+server.get('/api/users/:index', (req, res) => {
+    const { index } = req.params;
+    return res.json({ user: users[index]});
+});
+
+server.post('/api/users', (req, res) => {
+    const { name } = req.body;
+    users.push(name);
+    return res.json(users);
+});
+
+server.put('/api/users/:index', (req, res) => {
+    const { name } = req.body;
+    const { index } = req.params;
+    users[index] = name;
+    return res.json({ name: users[index] });
+});
+
+server.delete('/api/users/:index', (req, res) => {
+    const { index } = req.params;
+    users.splice(index, 1);
+    return res.json(users);
 });
 
 server.listen(3000);
