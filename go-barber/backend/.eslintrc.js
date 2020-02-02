a5f9e655af11417698e7445ddbcb88904f4596b7
Guilherme França <guii_real@hotmail.com>
2020-02-01 23:58:12 -0300

    Prettier and Eslint

--- go-barber/backend/.eslintrc.js
+++ go-barber/backend/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   rules: {
     "linebreak-style": 0,
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
