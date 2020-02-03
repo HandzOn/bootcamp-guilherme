b8d9117d211f62c63071e3f07b7a301d85a1f9bd
Guilherme França <guii_real@hotmail.com>
2020-02-03 01:38:36 -0300

    Updates

--- go-barber/backend/src/routes.js
+++ go-barber/backend/src/routes.js
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import AuthMiddleware from './app/middlewares/AuthMiddleware';
 
+import multerConfig from './config/multer';
+
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(AuthMiddleware.check);
+
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
