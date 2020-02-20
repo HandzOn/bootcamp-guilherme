f6aeb2f39dc32eee1cf0244e6f71109c940201ee
Guilherme França <guii_real@hotmail.com>
2020-02-20 01:52:03 -0300

    Validate business rules

--- go-barber/backend/src/config/database.js
+++ go-barber/backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: 'docker',
+    password: '150793',
     database: 'gobarber',
     define: {
         timestamps: true,
--- go-barber/backend/src/routes.js
+++ go-barber/backend/src/routes.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+
 import multer from 'multer';
+import multerConfig from './config/multer';
+
+import AuthMiddleware from './app/middlewares/AuthMiddleware';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
-
-import AuthMiddleware from './app/middlewares/AuthMiddleware';
-import multerConfig from './config/multer';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +21,7 @@ routes.use(AuthMiddleware.check);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
